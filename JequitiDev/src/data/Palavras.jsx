12a10b57e4af82302f7ad5e258bd65b644580802
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-05-10 10:03:43 -0300

    Revert "Movendo arquivos"
    This reverts commit 88fdff38b9219ab094e78d4e9a495393ffd53de2.

---
+++ JequitiDev/src/data/Palavras.jsx
@@ -0,0 +1,8 @@
+export const PalavrasLista = {
+
+    computador: ['mouse', 'teclado', 'processador', 'intel', 'nvidia'],
+    FrontEnd: ['html', 'javascript', 'reactjs', 'flexbox'],
+    Backend: ['bancodedados', 'crud', 'servidor', 'segurança'],
+    Internet: ['página', 'redesocial', 'google', 'chatgpt'],
+
+}
