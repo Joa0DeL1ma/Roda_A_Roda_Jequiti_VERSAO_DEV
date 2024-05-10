88fdff38b9219ab094e78d4e9a495393ffd53de2
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-05-10 09:23:13 -0300

    Movendo arquivos

--- JequitiDev/src/data/Palavras.jsx
+++
@@ -1,8 +0,0 @@
-export const PalavrasLista = {
-
-    computador: ['mouse', 'teclado', 'processador', 'intel', 'nvidia'],
-    FrontEnd: ['html', 'javascript', 'reactjs', 'flexbox'],
-    Backend: ['bancodedados', 'crud', 'servidor', 'segurança'],
-    Internet: ['página', 'redesocial', 'google', 'chatgpt'],
-
-}
